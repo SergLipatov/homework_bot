80adbc97c2f9ec9287eea9b9838722ea6090781a
SergLipatov <478997@mail.ru>
2025-04-08 07:38:05 +0400

    acquaintance

--- homework.py
+++ homework.py
@@ -1,11 +1,16 @@
-...
+import requests
+from dotenv import load_dotenv
+from telebot import TeleBot
+
+import os
+import time
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,47 +25,49 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+        raise Exception('Отсутствуют обязательные переменные окружения.')
 
 
 def send_message(bot, message):
-    ...
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(timestamp):
-    ...
+    return requests.get(ENDPOINT, headers=HEADERS, params={'from_date': {timestamp}})
 
 
 def check_response(response):
-    ...
-
+    pass
 
 def parse_status(homework):
-    ...
+    pass
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    #return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    check_tokens()
 
     # Создаем объект класса бота
-    bot = ...
+    bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
+    #print(HEADERS)
+    homework_statuses = get_api_answer(timestamp)
+    print(homework_statuses)
 
-    ...
 
     while True:
         try:
 
-            ...
+            homework_statuses = get_api_answer(timestamp)
+            print(homework_statuses)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+        break
 
 
 if __name__ == '__main__':
